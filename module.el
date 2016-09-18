74a12f4b847192a8225ed5536a821cad0a4fe747
igumus <ibrhm.gumus@gmail.com>
2016-09-18 06:34:30 +0300

    Added auto-{load,install-package}, reload-module features in module.el

--- module.el
+++ module.el
@@ -5,13 +5,18 @@
 
 (defconst *module-info-file* "info.el")
 (defconst *module-init-file* "init.el")
-(defconst *module-cfg-dir* "config")
-(defconst *module-lib-dir* "lib")
+(defconst *module-cfg-dir*   "config")
+(defconst *module-lib-dir*   "lib")
+
+(defvar active-module-list  nil "Holds active (loaded) module list")
+(defvar passive-module-list nil "Holds passive (not-loaded) module list")
 
 (defvar current-module-dir  nil "Current module directory")
 (defvar current-module-vsn  nil "Current module version")
 (defvar current-module-name nil "Current module name")
 (defvar current-module-desc nil "Current module desc")
+(defvar current-module-al   nil "Current module auto load")
+(defvar current-module-api  nil "Current module need auto package installation")
 
 (defun module-name (module)
   (setq current-module-name module))
@@ -22,13 +27,24 @@
 (defun module-desc (desc)
   (setq current-module-desc desc))
 
+(defun module-api ()
+  (setq current-module-api t))
+
+(defun module-al ()
+  (setq current-module-al t))
+
 (defun clear-module-info ()
   (setq current-module-name nil)
-  (setq current-module-vsn nil)
-  (setq current-module-desc nil))
+  (setq current-module-vsn  nil)
+  (setq current-module-desc nil)
+  (setq current-module-api  nil)
+  (setq current-module-al   nil)
+  )
 
-(defun module-lib-dir ()
-  (file-name-as-directory (concat current-module-dir *module-lib-dir*)))
+(defun module-lib-dir (&optional current-dir)
+  (unless current-dir (setq current-dir current-module-dir))
+  (when current-dir
+    (file-name-as-directory (concat current-dir *module-lib-dir*))))
 
 (defun module-cfg-dir ()
   (file-name-as-directory (concat current-module-dir *module-cfg-dir*)))
@@ -36,27 +52,58 @@
 (defun load-module-config (config-file)
   (load-file (concat (module-cfg-dir) config-file)))
 
-(defun load-module (module-dir)
+(defun load-module-config-with-package (pname cname)
+  (warn-module-package pname cname)
+  (when current-module-api
+    (package-install pname)
+    (load-module-config cname)))
+
+(defun find-module (search-name module-list)
+  (when module-list
+    (let* ((module (car module-list))
+	   (module-name (car module)))
+      (if (string-suffix-p search-name module-name t)
+	  module
+	(find-module search-name (cdr module-list))))))
+
+(defun activate-module (module-name)
+  (let* ((module (find-module module-name passive-module-list))
+	 (module-path (file-name-as-directory (cdr module))))
+    (if module
+	(progn
+	  (load-module module-path t)
+	  (add-to-list 'active-module-list module)
+	  (setq passive-module-list (remove module passive-module-list))
+	  )	  
+      (err-module (concat module-name " not found in passive module list")))))
+
+(defun load-module (module-dir &optional autoload)
   (info-module-loading module-dir)
   (let* ((module-dir (file-name-as-directory module-dir))
 	 (module-inf (concat module-dir *module-info-file*))
 	 (module-int (concat module-dir *module-init-file*)))
     (setq current-module-dir module-dir)
-    (clear-module-info)
 
     (if (file-exists-p module-inf)
-	(load-file module-inf)
+	(progn
+	  (load-file module-inf)
+	  (if (or current-module-al autoload)
+	      (progn
+		;; add lib dir of module when it exists.
+		(when (file-exists-p (module-lib-dir))
+		  (add-to-list 'load-path (module-lib-dir)))
+		
+		;; load init.el file of module when it exists.
+		(when (file-exists-p module-int)
+		  (load-file module-int))
+		(add-to-list 'active-module-list (cons current-module-name module-dir))
+		)
+	    (add-to-list 'passive-module-list (cons current-module-name module-dir)))
+	  )
       (err-module-info module-dir))
-
-    ;; add lib dir of module when it exists.
-    (when (file-exists-p (module-lib-dir))
-      (add-to-list 'load-path (module-lib-dir)))
-
-    ;; load init.el file of module when it exists.
-    (when (file-exists-p module-int)
-      (load-file module-int))
-
-    (setq current-module-dir nil)))
+    
+    (setq current-module-dir nil)
+    (clear-module-info)))
 
 (defun module-create-files-fn (module-name module-dir)
   (let ((files (mapcar (lambda (file) (concat module-dir file)) (list *module-info-file* *module-init-file*))))
